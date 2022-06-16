7bfada0b720285d246f92fd3b658f173c118a3cf
DESKTOP-3ED7E68\user <xenobiadexyuki@icloud.com>
2022-06-16 09:46:28 +0900

    通常ステージの追加、プレイヤースクリプト作成途中、ボム（見た目）の追加

--- Bomber Man/Assets/Block/BlockField.cs
+++ Bomber Man/Assets/Block/BlockField.cs
@@ -6,35 +6,59 @@ public class BlockField : MonoBehaviour
 {
     enum Block
     {
-        None,
-        Floor,
-        Break,
-        Wall,
+        //ブロックの順番
+        None,  //0
+        Floor, //1は床
+        Break, //2は壊せるブロック
+        Wall,　//3は壁
 
         Max
     }
 
     [SerializeField] private GameObject[] Prefab = null;
 
+    //13×11フィールド
+    const int FIELD_SIZE_X = 15;
+    const int FIELD_SIZE_Y = 13;
+
     static readonly int[,] FIELD = new int[,]
     {
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,},
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,},
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,},
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,},
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,},
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,},
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,},
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,},
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,},
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,},
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,},
+        { 3,3,3,3,3,3,3,3,3,3,3,3,3,3,3, },
+        { 3,0,0,0,0,0,0,0,0,0,0,0,0,0,3, },
+        { 3,0,3,0,3,0,3,0,3,0,3,0,3,0,3, },
+        { 3,0,0,0,0,0,0,0,0,0,0,0,0,0,3, },
+        { 3,0,3,0,3,0,3,0,3,0,3,0,3,0,3, },
+        { 3,0,0,0,0,0,0,0,0,0,0,0,0,0,3, },
+        { 3,0,3,0,3,0,3,0,3,0,3,0,3,0,3, },
+        { 3,0,0,0,0,0,0,0,0,0,0,0,0,0,3, },
+        { 3,0,3,0,3,0,3,0,3,0,3,0,3,0,3, },
+        { 3,0,0,0,0,0,0,0,0,0,0,0,0,0,3, },
+        { 3,0,3,0,3,0,3,0,3,0,3,0,3,0,3, },
+        { 3,0,0,0,0,0,0,0,0,0,0,0,0,0,3, },
+        { 3,3,3,3,3,3,3,3,3,3,3,3,3,3,3, },
     };
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //フィールドブロックを生成
+        float ofsX = -(FIELD_SIZE_X - 1) * 0.5f;
+        float ofsY = -(FIELD_SIZE_Y - 1) * 0.5f;
+        for (int y = 0; y < FIELD_SIZE_Y; y++)
+        {
+            for (int x = 0; x < FIELD_SIZE_X; x++)
+            {
+                GameObject FloorObj = Instantiate<GameObject>(Prefab[(int)Block.Floor]);
+                FloorObj.transform.localPosition = new Vector3(x + ofsX, -0.5f, y + ofsY);
+                int none = FIELD[y, x];
+                if (none == 0)
+                {
+                    continue;
+                }
+                GameObject newObj = Instantiate<GameObject>(Prefab[FIELD[y, x]]);
+                newObj.transform.localPosition = new Vector3(x + ofsX, 0.5f, y + ofsY);
+            }
+        }
     }
 
     // Update is called once per frame
