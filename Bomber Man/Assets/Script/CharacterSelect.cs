c08ceb752396593771ef61d5022f77da5719e407
Rumieru <nkc.takuki.nakasima@gmail.com>
2022-06-23 12:12:09 +0900

    セレクト画面

---
+++ Bomber Man/Assets/Script/CharacterSelect.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelect : MonoBehaviour
+{
+    int selectCharaNumber;
+
+    void Start()
+    {
+        selectCharaNumber = 0;
+    }
+
+    public void OnClickA()
+    {
+        selectCharaNumber = 0;
+    }
+    public void OnClickB()
+    {
+        selectCharaNumber = 1;
+    }
+
+
+    public void OnClickStart()
+    {
+        PlayerPrefs.SetInt("CHARA_NUMBER", selectCharaNumber);
+        SceneManager.LoadScene("GameScene");
+    }
+}
