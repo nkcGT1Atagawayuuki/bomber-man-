681595dd658c885ed5543e5478cba8e81de4f930
nkc-asano <doumo.fff@icloud.com>
2022-07-08 14:43:57 +0900

    遷移とステージ

--- Bomber Man/Assets/Script/StageSelect.cs
+++ Bomber Man/Assets/Script/StageSelect.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StageSelect : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Input.GetKey(KeyCode.Return))
+        {
+            SceneManager.LoadScene("stage0");
+        }
     }
 
     // Update is called once per frame
