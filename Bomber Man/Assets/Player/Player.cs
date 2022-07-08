75ae84ec732cd972db833d97933cc5af3022e5ea
DESKTOP-3ED7E68\user <xenobiadexyuki@icloud.com>
2022-07-08 11:15:01 +0900

    Playerのコントローラー修正

--- Bomber Man/Assets/Player/Player.cs
+++ Bomber Man/Assets/Player/Player.cs
@@ -4,33 +4,80 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] Animator animator;
-    public float Speed = 3.0f;
+    [SerializeField] private Animator animator = null;
+
+    private int _controlX = 0;
+    private int _controlZ = 0;
+
+    private float _angle = 0.0f;
+
+    static readonly int[,] ROTATION = new int[,]
+    {
+        {3,2,1},
+        {4,-1,0},
+        {5,6,7},
+    };
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("up"))
+        
+
+        float forward = 0.0f;
+
+        _controlX = 0;
+        _controlZ = 0;
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += transform.forward* Speed * Time.deltaTime;
+            _controlZ++;
         }
-        if (Input.GetKey("down"))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position -= transform.forward * Speed * Time.deltaTime;
+            _controlZ--;
         }
-        if (Input.GetKey("right"))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += transform.right * Speed * Time.deltaTime;
+            _controlX++;
         }
-        if (Input.GetKey("left"))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position -= transform.right * Speed * Time.deltaTime;
+            _controlX--;
         }
+
+        if (_controlX != 0 || _controlZ != 0)
+        {
+            forward = 1.0f;
+        }
+        animator.SetFloat("WalkF", forward, 0.1f, Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        //ˆÚ“®
+        float speed = 3.0f;
+        float deltaX = 0.0f;
+        float deltaZ = 0.0f;
+        if(_controlX != 0)
+        {
+            deltaX += Time.deltaTime * _controlX * speed;
+        }
+        if (_controlZ != 0)
+        {
+            deltaZ += Time.deltaTime * _controlZ * speed;//
+        }
+        transform.localPosition += new Vector3(deltaX, 0.0f, deltaZ);
+
+        int controlAngle = ROTATION[_controlZ + 1, _controlX + 1];
+        if (controlAngle >= 0)
+        {
+            _angle = controlAngle * 360.0f / 8.0f + 90.0f;
+        }
+        transform.localEulerAngles = new Vector3(0.0f, _angle, 0.0f);
     }
 }
