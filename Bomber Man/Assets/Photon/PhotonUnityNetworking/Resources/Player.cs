a5f1a0a28d3f708749f0e9e3ccb562e2b74d9a1e
DESKTOP-3ED7E68\user <xenobiadexyuki@icloud.com>
2022-07-19 16:03:46 +0900

    playerのスクリプトの変更

--- Bomber Man/Assets/Photon/PhotonUnityNetworking/Resources/Player.cs
+++ Bomber Man/Assets/Photon/PhotonUnityNetworking/Resources/Player.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviourPunCallbacks
 {
     [SerializeField] private Animator animator = null;
 
