7237a7c7ca85a98db310c6d8095d625dcc7b1130
Rumieru <nkc.takuki.nakasima@gmail.com>
2022-07-21 11:29:20 +0900

    タイトル終了

--- Bomber Man/Assets/Title/GameSystem.cs
+++ Bomber Man/Assets/Title/GameSystem.cs
@@ -10,4 +10,14 @@ public class GameSystem : MonoBehaviour
     {
         SceneManager.LoadScene("douki");
     }
+    public void EndGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+		Application.OpenURL("http://www.yahoo.co.jp/");
+#else
+		Application.Quit();
+#endif
+    }
 }
