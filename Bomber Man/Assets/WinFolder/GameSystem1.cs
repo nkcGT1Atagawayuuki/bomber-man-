e6b046caa44b4d6529706288aa776c5e76a35e45
Rumieru <nkc.takuki.nakasima@gmail.com>
2022-07-21 12:14:29 +0900

    title1ですがwin画面です

---
+++ Bomber Man/Assets/WinFolder/GameSystem1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSystem1 : MonoBehaviour
+{
+    //　スタートボタンを押したら実行する
+    public void titleGame()
+    {
+        SceneManager.LoadScene("Title");
+    }
+    public void RetryGame()
+    {
+        SceneManager.LoadScene("douki");
+    }
+
+    public void EndGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+		Application.OpenURL("http://www.yahoo.co.jp/");
+#else
+		Application.Quit();
+#endif
+    }
+}
