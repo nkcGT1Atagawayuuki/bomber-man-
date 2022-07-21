d57a8f50d187ec49b0e8313fff9f397e24a7abf8
Rumieru <nkc.takuki.nakasima@gmail.com>
2022-07-21 11:19:29 +0900

    タイトル

---
+++ Bomber Man/Assets/Title/GameSystem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour
+{
+    //　スタートボタンを押したら実行する
+    public void StartGame()
+    {
+        SceneManager.LoadScene("douki");
+    }
+}
